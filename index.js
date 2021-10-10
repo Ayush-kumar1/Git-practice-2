1b89ad989ce79e12e56f8003a3ab3ae94848cfc8
Ayush <ayush.ak5555@gmail.com>
2021-10-10 19:58:02 +0530

    Bad commit reverted

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var a = 5;
 console.log(a);
 console.log(a);
 
-var b = 7;
-console.log(b);
+// var b = 7;
+// console.log(b);
 
-var c = 9;
-console.log(c);
-
-var d = 12;
-console.log(d);
+// var c = 9;
+// console.log(c);
